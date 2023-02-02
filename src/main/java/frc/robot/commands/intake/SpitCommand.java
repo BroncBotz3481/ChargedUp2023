58039234664df896dc3a289626bd28e7afe5a395
Zamoa0714 <samuelzhao0714@gmail.com>
2023-02-02 17:32:50 -0600

    SpitCommand

--- src/main/java/frc/robot/commands/intake/SpitCommand.java
+++ src/main/java/frc/robot/commands/intake/SpitCommand.java
@@ -5,24 +5,36 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.intake.IntakePolicy;
 
 public class SpitCommand extends CommandBase {
   /** Creates a new SpitCommand. */
-  public SpitCommand() {
+  IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
+
+  public SpitCommand(IntakeSubsystem subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_IntakeSubsystem = subsystem;
+    addRequirements(m_IntakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_IntakeSubsystem.stopIntake();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_IntakeSubsystem.runIntake(1);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_IntakeSubsystem.stopIntake();
+  }
 
   // Returns true when the command should end.
   @Override
