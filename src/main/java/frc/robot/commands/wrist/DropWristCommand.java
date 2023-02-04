8bd8ec7571ada21b3da7080b1864cbe6694fc6af
loocyx <lucy.xie19@gmail.com>
2023-02-04 13:01:11 -0600

    comments added to wrist commands

--- src/main/java/frc/robot/commands/wrist/DropWristCommand.java
+++ src/main/java/frc/robot/commands/wrist/DropWristCommand.java
@@ -7,36 +7,37 @@ package frc.robot.commands.wrist;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.wrist.WristSubsystem;;
 
-public class DropWristCommand extends CommandBase {
+//creates DropWristCommand class and gets the methods and other attributes from CommandBase
+public class DropWristCommand extends CommandBase { 
   /** Creates a new WristCommand. */
-  WristSubsystem m_WristSubsystem = new WristSubsystem();
-  public DropWristCommand(WristSubsystem subsystem) {
-    m_WristSubsystem = subsystem;
+  WristSubsystem m_WristSubsystem = new WristSubsystem();   //creates new object m_WristSubsystem
+  public DropWristCommand(WristSubsystem subsystem) {     //constructor
+    m_WristSubsystem = subsystem;                       //allows object m_WristSubsystem to be used in other methods in this class
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_WristSubsystem);
   }
 
   // Called when the command is initially scheduled.
-  @Override
+  @Override             //creates method that functions outside of the parent function's method of the same name
   public void initialize() {
-    m_WristSubsystem.stopMotor();
+    m_WristSubsystem.stopMotor();         //do not drop wrist when initialized
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_WristSubsystem.setMotor(-5000);;
+    m_WristSubsystem.setMotor(-5000);;      //sets wrist motor to -5000 so that it drops
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_WristSubsystem.stopMotor();
+    m_WristSubsystem.stopMotor();       //stops motor     
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return false;       //returns false
   }
 }
--- src/main/java/frc/robot/commands/wrist/StopWristCommand.java
+++ src/main/java/frc/robot/commands/wrist/StopWristCommand.java
@@ -13,33 +13,33 @@ public class StopWristCommand extends CommandBase {
   /** 
    * @param subsystem
    * Creates a new StopWristCommand. */
-  public StopWristCommand(WristSubsystem subsystem) {
+  public StopWristCommand(WristSubsystem subsystem) {                //constructor method
     // Use addRequirements() here to declare subsystem dependencies.
-    m_WristSubsystem = subsystem;
+    m_WristSubsystem = subsystem;         //allows private variable to be used in other methods in the class
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_WristSubsystem.stopMotor();
+    m_WristSubsystem.stopMotor();          //calls stop motor when command is initialized
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_WristSubsystem.stopMotor();
+    m_WristSubsystem.stopMotor();              //calls stops motor when command is executed
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_WristSubsystem.stopMotor();
+    m_WristSubsystem.stopMotor();               //stops wrist motor
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return false;                     //returns false
   }
 }
