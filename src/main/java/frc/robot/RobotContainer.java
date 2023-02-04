f2eb14732e4eac79b8e955a62dc9f19d18ce3e2e
loocyx <lucy.xie19@gmail.com>
2023-02-04 13:35:06 -0600

    cleaned up spacing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -83,9 +83,9 @@ public class RobotContainer {
     m_wristSubsystem.setDefaultCommand(new StopWristCommand(m_wristSubsystem));//Replace with turtle command
 
     //m_operatorController.blankTrigger().whileTrue(new CrapMyselfCommand(m_iHateLife));
-      m_operatorController.leftTrigger().whileTrue(new SpitCommand(m_intakeSubsystem)); //bind spit command while left trig pressed
-      m_operatorController.leftBumper().whileTrue(new SpinCommand(m_intakeSubsystem)); //bind spin command while left bumper pressed
-      m_operatorController.rightTrigger().whileTrue(new DropWristCommand(m_wristSubsystem)); //bind drop wrist while right trig pressed
+    m_operatorController.leftTrigger().whileTrue(new SpitCommand(m_intakeSubsystem)); //bind spit command while left trig pressed
+    m_operatorController.leftBumper().whileTrue(new SpinCommand(m_intakeSubsystem)); //bind spin command while left bumper pressed
+    m_operatorController.rightTrigger().whileTrue(new DropWristCommand(m_wristSubsystem)); //bind drop wrist while right trig pressed
   }
 
     
