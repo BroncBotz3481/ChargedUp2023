64bb89555c777cc452233104319f162850cb6958
Zamoa0714 <samuelzhao0714@gmail.com>
2023-02-14 17:58:57 -0600

    Indention

--- src/main/java/frc/robot/subsystems/bogey/BogeySubsystem.java
+++ src/main/java/frc/robot/subsystems/bogey/BogeySubsystem.java
@@ -14,82 +14,82 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.bogey.BogeyPolicy.PIDF;
 
 public class BogeySubsystem extends SubsystemBase {
-  /**
-   * SparkMax for the bogey motor
-   */
-  private final CANSparkMax bogeyMotor;
-  /**
-   * SparxMaxPIDController from SparkMax
-   */
-  private final SparkMaxPIDController PIDController;
-  /**
-   * Relative encoder from the bogey motor
-   */
-  private final RelativeEncoder bogeyEncoder;
+    /**
+     * SparkMax for the bogey motor
+     */
+    private final CANSparkMax bogeyMotor;
+    /**
+     * SparxMaxPIDController from SparkMax
+     */
+    private final SparkMaxPIDController PIDController;
+    /**
+     * Relative encoder from the bogey motor
+     */
+    private final RelativeEncoder bogeyEncoder;
 
-  /**
-   * The constructor initializes the motor, pidcontroller, encoder, and the PIDF constants
-   */
-  public BogeySubsystem() {
-    bogeyMotor = new CANSparkMax(BogeyPolicy.BOGEY_ID_PORT, MotorType.kBrushless);
-    bogeyMotor.restoreFactoryDefaults();
-    PIDController = bogeyMotor.getPIDController();
-    bogeyEncoder = bogeyMotor.getEncoder();
-    bogeyMotor.setIdleMode(IdleMode.kBrake);
+    /**
+     * The constructor initializes the motor, pidcontroller, encoder, and the PIDF constants
+     */
+    public BogeySubsystem() {
+        bogeyMotor = new CANSparkMax(BogeyPolicy.BOGEY_ID_PORT, MotorType.kBrushless);
+        bogeyMotor.restoreFactoryDefaults();
+        PIDController = bogeyMotor.getPIDController();
+        bogeyEncoder = bogeyMotor.getEncoder();
+        bogeyMotor.setIdleMode(IdleMode.kBrake);
 
-    set(PIDF.PROPORTION, PIDF.INTEGRAL, PIDF.DERIVATIVE, PIDF.FEEDFORWARD, PIDF.INTEGRAL_ZONE);
-  }
+        set(PIDF.PROPORTION, PIDF.INTEGRAL, PIDF.DERIVATIVE, PIDF.FEEDFORWARD, PIDF.INTEGRAL_ZONE);
+    }
 
-  /**
-   * Sets the spark max closed loop PIDF values
-   *
-   * @param p  Proportional gain constant
-   * @param i  Integral gain constant
-   * @param d  Derivative constant
-   * @param f  Feedforward constant
-   * @param iz Integral Zone
-   */
-  public void set(double p, double i, double d, double f, double iz) {
-    PIDController.setP(p);
-    PIDController.setI(i);
-    PIDController.setD(d);
-    PIDController.setFF(f);
-    PIDController.setIZone(iz);
-  }
+    /**
+     * Sets the spark max closed loop PIDF values
+     *
+     * @param p  Proportional gain constant
+     * @param i  Integral gain constant
+     * @param d  Derivative constant
+     * @param f  Feedforward constant
+     * @param iz Integral Zone
+     */
+    public void set(double p, double i, double d, double f, double iz) {
+        PIDController.setP(p);
+        PIDController.setI(i);
+        PIDController.setD(d);
+        PIDController.setFF(f);
+        PIDController.setIZone(iz);
+    }
 
-  /**
-   * Moves the arm with voltage
-   *
-   * @param power The power used to move the bogey motor
-   */
-  public void moveArm(double power) {
-    BogeyPolicy.bogeyPower = power;
-    bogeyMotor.set(BogeyPolicy.bogeyPower);
-  }
+    /**
+     * Moves the arm with voltage
+     *
+     * @param power The power used to move the bogey motor
+     */
+    public void moveArm(double power) {
+        BogeyPolicy.bogeyPower = power;
+        bogeyMotor.set(BogeyPolicy.bogeyPower);
+    }
 
-  /**
-   * @param targetPosition The set position for the PIDF Loop
-   */
-  public void runPID(double targetPosition) {
-    BogeyPolicy.setPosition = targetPosition;
-    PIDController.setReference(BogeyPolicy.setPosition, ControlType.kPosition);
-  }
+    /**
+     * @param targetPosition The set position for the PIDF Loop
+     */
+    public void runPID(double targetPosition) {
+        BogeyPolicy.setPosition = targetPosition;
+        PIDController.setReference(BogeyPolicy.setPosition, ControlType.kPosition);
+    }
 
-  /**
-   * Stops the arm with voltage
-   */
-  public void stopArm() {
-    moveArm(0);
-  }
+    /**
+     * Stops the arm with voltage
+     */
+    public void stopArm() {
+        moveArm(0);
+    }
 
-  /**
-   * Periodically gets the encoder velocity and position and puts it inside the policy class
-   */
-  @Override
-  public void periodic() {
-    BogeyPolicy.encoderVelocity = bogeyEncoder.getVelocity();
-    BogeyPolicy.encoderPosition = bogeyEncoder.getPosition();
-  }
+    /**
+     * Periodically gets the encoder velocity and position and puts it inside the policy class
+     */
+    @Override
+    public void periodic() {
+        BogeyPolicy.encoderVelocity = bogeyEncoder.getVelocity();
+        BogeyPolicy.encoderPosition = bogeyEncoder.getPosition();
+    }
 }
 
     
