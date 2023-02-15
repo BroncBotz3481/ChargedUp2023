2f576a6e60e78f17474de61bdc5f4a50544ddb7a
smartyvv <107887886+smartyvv@users.noreply.github.com>
2023-02-14 18:10:22 -0600

    Wrist Policy/Subsystem Javadoc

--- src/main/java/frc/robot/subsystems/wrist/WristPolicy.java
+++ src/main/java/frc/robot/subsystems/wrist/WristPolicy.java
@@ -5,14 +5,30 @@
 package frc.robot.subsystems.wrist;
 
 /**
- * Add your docs here.
- */
+ * This is the Wrist policy, holding all algorithmic functions for the Wrist Subsystem and Commands.
+ * Holds all important values from the subsystem
+ **/
 public final class WristPolicy
 {
 
+  /**
+   * CAN ID Port for Wrist Motor
+   */
   public static final int    WRIST_ID_PORT = 10;
+  /**
+   * Power set to Wrist Motor
+   */
   public static       double power;
+  /**
+   * Position set to Wrist Motor
+   */
   public static       double setPosition;
+  /**
+   * Velocity set to encoder
+   */
   public static       double encoderVelocity;
+  /**
+   * Position set to encoder
+   */
   public static       double encoderPosition;
 }
