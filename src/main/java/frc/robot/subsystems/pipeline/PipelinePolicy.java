994716dd0603a8a90fbd53ef1d901e52d93efd16
svs2005 <56053155+svs2005@users.noreply.github.com>
2023-03-02 19:04:52 -0600

    Vision?
    Omar pls check this :ambulance:

--- src/main/java/frc/robot/subsystems/pipeline/PipelinePolicy.java
+++ src/main/java/frc/robot/subsystems/pipeline/PipelinePolicy.java
@@ -20,6 +20,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,97 +34,101 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.EstimatedRobotPose;
 
-
-
 /**
- * This is the Pipeline policy, holding all algorithmic functions for the psuedo Pipeline Subsystem and Commands. Holds
+ * This is the Pipeline policy, holding all algorithmic functions for the psuedo
+ * Pipeline Subsystem and Commands. Holds
  * all important values from the subsystem
  **/
-public final class PipelinePolicy
-{
+public final class PipelinePolicy {
+
+  /**
+   * The result of the pipeline
+   */
+  public static PhotonPipelineResult result;
+
+  /**
+   * The AprilTag target
+   */
+  public static PhotonTrackedTarget target;
+
+  /**
+   * First choice photon position from camera
+   */
+  public static PhotonPoseEstimator bestPhotonPoseEstimator;
+
+  /**
+   * Alternative second choice photon position from camera
+   */
+  public static PhotonPoseEstimator altPhotonPoseEstimator;
 
   /**
    * The current position of the robot on the field
    */
-  public static Translation3d       currentPosition;
+  public static Translation3d currentPosition;
+
+  /**
+   * The camera the robot uses
+   */
+  public static PhotonCamera camera;
+
   /**
    * The april tag locations on the field.
    */
-
-  public static GlobalMeasurement bestMeasurement;
-
-  public static PhotonPoseEstimator bestPhotonPoseEstimator;
-
-  public static PhotonPoseEstimator altPhotonPoseEstimator;
-
-  public static Pose2d pEST;
-
-  public static boolean isConnected;
-
   public static AprilTagFieldLayout aprilTagFieldLayout;
+
   /**
    * Is true if Apriltag is in camera's sight, false if not
    */
-  public static boolean             targetExists;
+  public static boolean targetExists;
+
   /**
-   * This double array holds yaw,pitch,area,and skew respectively in that order of the Apriltag
+   * This double array holds yaw,pitch,area,and skew respectively in that order of
+   * the Apriltag
    */
-  public static double[]            pipeline;
+
+  public static double[] pipeline;
+
   /**
    * A Transform3d object that represents a transformation for a Pose3D
    */
-  public static Transform3d         cameraToTargetTransformation;
+  public static Transform3d cameraToTargetTransformation;
 
-
-  public static final Transform3d robotToCam =
-  new Transform3d(
+  /**
+   * Camera to robot position
+   */
+  public static final Transform3d robotToCam = new Transform3d(
       new Translation3d(0.0, Units.inchesToMeters(0), Units.inchesToMeters(0)),
-      new Rotation3d(0.0, 0.0, 0.0)); 
+      new Rotation3d(0.0, 0.0, 0.0));
 
   /**
    * List that holds the corners of the Apriltag
    */
-  public static List                corners;
-
+  public static List corners;
 
   /**
    * Updates the currentPosition of the robot based on
-   *
-   * @param bestCameraToTarget relative location of the robot to the AprilTag target
-   * @param fiducialId         AprilTag ID
+   * 
+   * @param fiducialId AprilTag ID
    */
-  public static void updateLocation(Transform3d bestCameraToTarget, PhotonPoseEstimator ppe, int fiducialId, PhotonCamera cam)
-  {
-    Pose3d aprilTag = null; //FIX THIS LATER
+  public static void updateLocation(int fiducialId) {
+    Pose3d aprilTag = new Pose3d();
     if (aprilTagFieldLayout.getTagPose(fiducialId).isPresent()) {
       aprilTag = aprilTagFieldLayout.getTagPose(fiducialId).get();
     }
     currentPosition = aprilTag.getTranslation().minus(cameraToTargetTransformation.getTranslation());
-    bestPhotonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam, robotToCam);
-    altPhotonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam, robotToCam);
-
-
-    /**TODO:
-     * I need to pass in pEST into bestPhotonPoseEstimator
-     * I need to add in GlobalMeasurement
-     * OMAR PLS DO NOT JUDGE THIS, IT IS RAW!!
-     */
-
-    /*public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose, Pose2d pEST) {
-      pEST.setReferencePose(prevEstimatedRobotPose);
-      return pEST.update();
-    }
-    */
-
-
-
 
   }
-  
 
-  public void aVisionProcessingAlgorithmThatAutomagicallyWorks()
-  {
-    //magic ensues...
+  /**
+   * Recieves previous estimated robot position and sets reference positions
+   * 
+   * @param prevEstimatedRobotPose Previous estimated robot position
+   * @return updated best photon position
+   */
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    bestPhotonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+    return bestPhotonPoseEstimator.update();
+
   }
 
 }
--- src/main/java/frc/robot/subsystems/pipeline/PipelineSubsystem.java
+++ src/main/java/frc/robot/subsystems/pipeline/PipelineSubsystem.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.pipeline;
 
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -22,53 +25,43 @@ import edu.wpi.first.math.geometry.Transform3d;
 /**
  * The Psuedo Pipeline subsystem on the robot
  */
-public class PipelineSubsystem extends SubsystemBase
-{
+public class PipelineSubsystem extends SubsystemBase {
+  /**
+   * Initializes the camera, best/alt photon position estimator, apriltag field
+   * layout
+   */
+  private PipelineSubsystem() {
+    PipelinePolicy.camera = new PhotonCamera("Integrated_Webcam");
+    PipelinePolicy.bestPhotonPoseEstimator = new PhotonPoseEstimator(PipelinePolicy.aprilTagFieldLayout,
+        PoseStrategy.CLOSEST_TO_REFERENCE_POSE, PipelinePolicy.camera, PipelinePolicy.robotToCam);
 
-  /**
-   * The camera the robot uses
-   */
-  private final PhotonCamera         camera;
-  /**
-   * The result of the pipeline
-   */
-  private       PhotonPipelineResult result;
-  /**
-   * The AprilTag target
-   */
-  private       PhotonTrackedTarget  target;
-
-  /**
-   * Initializes the camera the robot uses
-   */
-  private PipelineSubsystem()
-  {
-    camera = new PhotonCamera("Integrated_Webcam");
-    try
-    {
+    PipelinePolicy.altPhotonPoseEstimator = new PhotonPoseEstimator(PipelinePolicy.aprilTagFieldLayout,
+        PoseStrategy.CLOSEST_TO_REFERENCE_POSE, PipelinePolicy.camera, PipelinePolicy.robotToCam);
+    try {
       PipelinePolicy.aprilTagFieldLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
-    } catch (Exception ignored)
-    {
+    } catch (Exception ignored) {
 
     }
 
   }
 
   /**
-   * Periodically gets the pipeline result and pipeline target, then sends a boolean to the pipeline policy if the
-   * camera sees a AprilTag, then sends the position of the target , then sends the translation of the camera, then
+   * Periodically gets the pipeline result and pipeline target, then sends a
+   * boolean to the pipeline policy if the
+   * camera sees a AprilTag, then sends the position of the target , then sends
+   * the translation of the camera, then
    * sends the corners of the AprilTag all to the policy class
+   * updates location
    */
   @Override
-  public void periodic()
-  {
-    result = camera.getLatestResult();
-    PipelinePolicy.targetExists = result.hasTargets();
-    target = result.getBestTarget();
-    PipelinePolicy.pipeline = new double[]{target.getYaw(), target.getPitch(), target.getArea(), target.getSkew()};
-    PipelinePolicy.cameraToTargetTransformation = target.getBestCameraToTarget();
-    PipelinePolicy.corners = target.getDetectedCorners();
-    //PipelinePolicy.updateLocation(target.getBestCameraToTarget(), target.getFiducialId());
+  public void periodic() {
+    PipelinePolicy.result = PipelinePolicy.camera.getLatestResult();
+    PipelinePolicy.targetExists = PipelinePolicy.result.hasTargets();
+    PipelinePolicy.target = PipelinePolicy.result.getBestTarget();
+    PipelinePolicy.pipeline = new double[] { PipelinePolicy.target.getYaw(), PipelinePolicy.target.getPitch(),
+    PipelinePolicy.target.getArea(), PipelinePolicy.target.getSkew() };
+    PipelinePolicy.cameraToTargetTransformation = PipelinePolicy.target.getBestCameraToTarget();
+    PipelinePolicy.corners = PipelinePolicy.target.getDetectedCorners();
+    PipelinePolicy.updateLocation(PipelinePolicy.target.getFiducialId());
   }
 }
-
