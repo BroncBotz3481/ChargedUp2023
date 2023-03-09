bd3e07a174a8b285a8f2061f5ca310feb8836d21
svs2005 <56053155+svs2005@users.noreply.github.com>
2023-03-08 20:43:28 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 
+import swervelib.parser.PIDFConfig;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
@@ -121,6 +122,24 @@ public final class Constants
         public static int lower = 5;
       }
     }
+  }
+
+  public static final class Auton
+  {
+
+    public static final PIDFConfig xAutoPID     = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig yAutoPID     = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
+
+    public static final double MAX_SPEED        = 2;
+    public static final double MAX_ACCELERATION = 2;
+  }
+
+  public static final class Drivebase
+  {
+
+    // Hold time on motor brakes when disabled
+    public static final double WHEEL_LOCK_TIME = 10; // seconds
   }
 
 }
